3b55e32396767b9100fee1952347ff50c98567cf
Patricia Nykaza <pnykaza@calstatela.edu>
2019-01-22 20:06:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
 // add solution here
-const musicians = ["John", "Paul", "George", "Ringo"];
+// const musicians = ["John", "Paul", "George", "Ringo"];
+//
+// const instruments = ["guitar", "guitar", "bass", "drums"];
 
-const instruments = ["guitar", "guitar", "bass", "drums"];
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+  	array.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  var results = [];
+  var count = 0;
+  while (count < facts.length) {
+    results.push(facts[count] + "!!!");
+    count++;
+  }
+  return results;
+}
+
+function iLoveTheBeatles(num) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!")
+    num++;
+  } while (num < 15);
+  return array;
+}
